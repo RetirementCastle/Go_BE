1f16b6f5f94d7c58e4c705f71caec0d849dfb974
root <joacampora@unal.edu.co>
2018-11-03 09:22:56 -0500

    hosting mysql

--- app.go
+++ app.go
@@ -20,7 +20,7 @@ type App struct {
 }
 
 func (a *App) Initialize(user, password, dbname string) {
-	connectionString := fmt.Sprintf("%s:%s@/%s", user, password, dbname)
+	connectionString := fmt.Sprintf("%s:%s@tcp(107.180.54.252)/%s", user, password, dbname)
 
 	var err error
 	a.DB, err = sql.Open("mysql", connectionString)
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ package main
 
 func main() {
 	a := App{}
-	a.Initialize("root", "12345678", "NursingHomes")
-
+	//a.Initialize("root", "12345678", "NursingHomes")
+	a.Initialize("generic_test", "Arquitectura2018", "retirementcastle")
 	a.Run(":8087")
 }
